f0f6933b9c3967d8d806ed13fb278947b4239dfd
Chandrika Deb <chandrikadeb7@gmail.com>
2020-05-28 11:58:33 +0530

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,6 @@
+#  this is just  a sample config file.You need to edit this file.You need to get these details from your Twitter app settings.
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
